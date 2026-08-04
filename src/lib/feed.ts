a6017f8e8ec4d6dfdf160be9a56ed82400517f9b
Mahammad Sad <mahammadsad3002@gmail.com>
2026-08-04 20:02:43 +0530

    Phase 9: add localized RSS feed builder

---
+++ src/lib/feed.ts
@@ -0,0 +1,28 @@
+import rss from '@astrojs/rss';
+import { getLocalizedArticles } from '@lib/content';
+import { localizedFeedUrl, schemaLanguage } from '@lib/seo';
+import { getBrandName, getBrandTagline, SITE } from '@utils/constants';
+import type { Locale } from '../i18n';
+
+export async function createLocalizedFeed(locale: Locale) {
+  const articles = await getLocalizedArticles(locale);
+  const feedUrl = localizedFeedUrl(locale);
+
+  return rss({
+    title: getBrandName(locale),
+    description: getBrandTagline(locale),
+    site: `${SITE.url}/`,
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+    },
+    items: articles.map((article) => ({
+      title: article.data.title,
+      description: article.data.description,
+      pubDate: article.data.updated || article.data.date,
+      link: `${SITE.url}/${article.data.language}/articles/${article.data.urlSlug}/`,
+      author: article.data.author,
+      categories: [article.data.category, ...article.data.tags],
+    })),
+    customData: `<language>${schemaLanguage(locale)}</language><atom:link href="${feedUrl}" rel="self" type="application/rss+xml" />`,
+  });
+}
